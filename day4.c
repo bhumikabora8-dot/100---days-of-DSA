09401427a4a9f751bf969d84aba5a006587cd48a
bhumikabora8-dot <bhumikabora8@gmail.com>
2026-02-10 23:39:47 +0530

    Reverse an Array In-Place

--- day4.c
+++ day4.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+
 int main() {
     int n;
     scanf("%d", &n);
-    int arr[1000];
+
+    int arr[n];
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-    int left = 0;
-    int right = n - 1;
+
+    int left = 0, right = n - 1;
     while (left < right) {
         int temp = arr[left];
         arr[left] = arr[right];
@@ -15,8 +17,11 @@ int main() {
         left++;
         right--;
     }
+
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
+
     return 0;
 }
+
