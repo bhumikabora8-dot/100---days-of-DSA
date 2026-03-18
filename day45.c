ed87a8ca14b1418299c557e7057c71834e6411e2
bhumikabora8-dot <bhumikabora8@gmail.com>
2026-03-18 10:23:07 +0530

    Height of a Binary Tree

---
+++ day45.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Tree node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create new node
+struct Node* newNode(int data) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = data;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+// Function to calculate height
+int height(struct Node* root) {
+    if (root == NULL)
+        return 0;
+
+    int leftHeight = height(root->left);
+    int rightHeight = height(root->right);
+
+    return (leftHeight > rightHeight ? leftHeight : rightHeight) + 1;
+}
+
+// Build tree from level order
+struct Node* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1)
+        return NULL;
+
+    struct Node* nodes[n];
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == -1)
+            nodes[i] = NULL;
+        else
+            nodes[i] = newNode(arr[i]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (nodes[i] != NULL) {
+            int leftIndex = 2*i + 1;
+            int rightIndex = 2*i + 2;
+
+            if (leftIndex < n)
+                nodes[i]->left = nodes[leftIndex];
+            if (rightIndex < n)
+                nodes[i]->right = nodes[rightIndex];
+        }
+    }
+
+    return nodes[0];
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    int arr[N];
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    struct Node* root = buildTree(arr, N);
+
+    printf("%d", height(root));
+
+    return 0;
+}
