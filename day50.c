baffa24522b0888dfca990256cb7461fb6e6afa7
bhumikabora8-dot <bhumikabora8@gmail.com>
2026-03-24 21:42:45 +0530

    BST Search

---
+++ day50.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure of Node
+struct Node {
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+// Create new node
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+// Insert into BST
+struct Node* insert(struct Node* root, int data) {
+    if (root == NULL)
+        return newNode(data);
+
+    if (data < root->data)
+        root->left = insert(root->left, data);
+    else if (data > root->data)
+        root->right = insert(root->right, data);
+
+    return root;
+}
+
+// Search in BST
+int search(struct Node* root, int key) {
+    if (root == NULL)
+        return 0;
+
+    if (root->data == key)
+        return 1;
+    else if (key < root->data)
+        return search(root->left, key);
+    else
+        return search(root->right, key);
+}
+
+// Main function
+int main() {
+    int n, x;
+    scanf("%d", &n);
+
+    struct Node* root = NULL;
+
+    // Build BST
+    for (int i = 0; i < n; i++) {
+        int val;
+        scanf("%d", &val);
+        root = insert(root, val);
+    }
+
+    // Value to search
+    scanf("%d", &x);
+
+    if (search(root, x))
+        printf("Found\n");
+    else
+        printf("Not Found\n");
+
+    return 0;
+}
