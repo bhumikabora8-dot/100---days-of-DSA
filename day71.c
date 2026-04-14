8649ec9c1037e43388750a7cee81e8319b3904eb
bhumikabora8-dot <bhumikabora8@gmail.com>
2026-04-14 22:33:39 +0530

    Hash Table Using Quadratic Probing

---
+++ day71.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <string.h>
+
+#define EMPTY -1
+
+int table[100];
+
+int hash(int key, int m) {
+    return key % m;
+}
+
+void insert(int key, int m) {
+    int i = 0;
+    int index;
+
+    while (i < m) {
+        index = (hash(key, m) + i * i) % m;
+
+        if (table[index] == EMPTY) {
+            table[index] = key;
+            return;
+        }
+        i++;
+    }
+
+    printf("Hash table is full\n");
+}
+
+void search(int key, int m) {
+    int i = 0;
+    int index;
+
+    while (i < m) {
+        index = (hash(key, m) + i * i) % m;
+
+        if (table[index] == EMPTY) {
+            printf("NOT FOUND\n");
+            return;
+        }
+
+        if (table[index] == key) {
+            printf("FOUND\n");
+            return;
+        }
+        i++;
+    }
+
+    printf("NOT FOUND\n");
+}
+
+int main() {
+    int m, q;
+    scanf("%d", &m);   // size of hash table
+    scanf("%d", &q);   // number of operations
+
+    // initialize table
+    for (int i = 0; i < m; i++) {
+        table[i] = EMPTY;
+    }
+
+    char operation[10];
+    int key;
+
+    for (int i = 0; i < q; i++) {
+        scanf("%s %d", operation, &key);
+
+        if (strcmp(operation, "INSERT") == 0) {
+            insert(key, m);
+        } else if (strcmp(operation, "SEARCH") == 0) {
+            search(key, m);
+        }
+    }
+
+    return 0;
+}
